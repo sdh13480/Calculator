b36c4009bafcb4e22f5673aeb709a890e36fc779
loysl <loys.legrand@free.fr>
2024-10-12 09:40:46 +0200

    Ajout des fonctions pour l'addition et la soustraction - correction typo

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
 def add(first_term, second_term):
         return first_term + second_term
 
-def substract(first_term, second_term):
+def subtract(first_term, second_term):
         return first_term - second_term
